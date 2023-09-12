d1bfe610f5314b424c1c9a40df439840e8fb4ca0
Jinsu Kwak <jinsukwak2001@gmail.com>
2023-09-12 02:04:09 -0600

    add pokemon card UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import "./index.css";
 import axios from "axios";
+import PokeCard from "./components/PokeCard";
 
 function App() {
   const offset = 0;
-  const url = `https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=${offset}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=1000&offset=${offset}`;
 
   const [pokemons, setPokemons] = useState([]);
-
-  const fetchPokeData = async () => {
-    try {
-      const res = await axios.get(url);
-      setPokemons(res.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   useEffect(() => {
     fetchPokeData();
   }, []);
+
+  async function fetchPokeData() {
+    try {
+      const res = await axios.get(url);
+      setPokemons(res.data.results);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <article className="pt-6">
@@ -30,11 +31,13 @@ function App() {
       >
         Input form
       </header>
-      <section className="pt-6 flex flex-col justify-content item-center overflow-auto z-0">
+      <section className="pt-6 flex flex-col justify-content overflow-auto z-0 items-center">
         Pokemon
         <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
           {pokemons.length > 0 ? (
-            pokemons.map(({ url, name }, index) => <div>{name}</div>)
+            pokemons.map(({ url, name }, index) => (
+              <PokeCard key={url} url={url} name={name} />
+            ))
           ) : (
             <h2
               className="font-medium text-lg text-slate
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import LazyImage from "./LazyImage";
+
+export const PokeCard = ({ url, name }) => {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetchPokeDetailData();
+  }, []);
+
+  async function fetchPokeDetailData() {
+    try {
+      const res = await axios.get(url);
+      const pokeData = formatPokeData(res.data);
+      setPokemon(pokeData);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function formatPokeData(data) {
+    const { id, types, name } = data;
+    const pokeData = {
+      id,
+      name,
+      type: types[0].type.name,
+    };
+    return pokeData;
+  }
+
+  const bg = `bg-${pokemon?.type}`;
+  const border = `border-${pokemon?.type}`;
+  const text = `text-${pokemon?.type}`;
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+  return (
+    <React.Fragment>
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 items-center justify-between`}
+        >
+          <div
+            className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
+          >
+            #{pokemon.id.toString().padStart(3, "00")}
+          </div>
+          <div className={`w-full f-6 flex items-center justify-center`}>
+            <div
+              className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
+            >
+              <LazyImage url={img} alt={name} />
+            </div>
+          </div>
+          <div
+            className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium flex justify-center pt-1`}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default PokeCard;
